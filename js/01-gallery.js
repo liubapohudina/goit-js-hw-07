a652163c1e1069b37599aa2173f95c07ab6bc508
Liubov <drogaltseva92@gmail.com>
2023-10-23 22:00:32 +0200

    add images

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,18 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
+const listImage = document.querySelector(".gallery")
+const itemImage = galleryItems.map((item) => `  <li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"
+      />
+    </a>
+  </li>
+`)
+    .join();
+ listImage.insertAdjacentHTML("afterbegin", itemImage)
+//console.log(itemImage)
 console.log(galleryItems);
