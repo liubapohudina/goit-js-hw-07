5f68a4aee0e17d9d82bd05d50b14bac36639bde3
Liubov <drogaltseva92@gmail.com>
2023-10-24 21:15:21 +0200

    finish task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,18 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const listImage = document.querySelector(".gallery")
-const itemImage = galleryItems.map((item) => `  <li class="gallery__item">
-    <a class="gallery__link" href="${item.original}">
+const itemImage = galleryItems.map(({preview, original, description}) => `  <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
       <img
         class="gallery__image"
-        src="${item.preview}"
-        data-source="${item.original}"
-        alt="${item.description}"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
       />
     </a>
   </li>
 `)
     .join("");
- listImage.insertAdjacentHTML("afterbegin", itemImage)
-//console.log(itemImage)
+listImage.insertAdjacentHTML("afterbegin", itemImage)
+listImage.addEventListener("click", bigImage)
+const instance = basicLightbox.create(`<img src="">`, {
+  onShow: () => { document.addEventListener("keydown", closeImage) },
+  onClose: () => { document.removeEventListener("keydown", closeImage) }
+});
+ //console.log(instance.element())
+function bigImage(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") return;
+    instance.element().querySelector("img").src = event.target.dataset.source;
+  instance.show()
+  }
+function closeImage(event) {
+  if (event.code === "Escape") {
+    instance.close();
+  }
+}
 console.log(galleryItems);
+
+
+  
